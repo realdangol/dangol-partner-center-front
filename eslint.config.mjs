b0169bce7dcefd05006d7eb06c623a9fb9070e26
HyeBinYang <skdisk7368@gmail.com>
2025-04-12 21:45:50 +0900

    chore: extends dangol config package eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,25 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import dangolEsLintConfig from '@dangol-dev/front-eslint-config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    rules: {},
+  },
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')];
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', ...dangolEsLintConfig.extends),
+  {
+    rules: {
+      ...dangolEsLintConfig.rules,
+    },
+  },
+];
 
 export default eslintConfig;
