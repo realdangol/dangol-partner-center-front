4b690c6215dad01d43b6f60f6fbca741ebc3e750
HyeBinYang <skdisk7368@gmail.com>
2025-04-19 04:20:55 +0900

    feat: Button 컴포넌트 추가

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from './Button';
+
+const Icon = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M5 12H19M12 19V5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+const meta: Meta<typeof Button> = {
+  title: 'common/Button',
+  component: Button,
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['fillPrimary', 'fillNeutral', 'outlinePrimary', 'outlineNeutral'],
+      description: '버튼 모양',
+    },
+    size: {
+      control: 'select',
+      options: ['large', 'medium', 'small'],
+      description: '버튼 크기',
+    },
+    leftIcon: {
+      control: 'select',
+      options: ['None', 'Icon'],
+      mapping: {
+        None: null,
+        Icon: <Icon />,
+      },
+    },
+    rightIcon: {
+      control: 'select',
+      options: ['None', 'Icon'],
+      mapping: {
+        None: null,
+        Icon: <Icon />,
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Usage: Story = {
+  args: {
+    children: 'Button',
+    disabled: true,
+  },
+};
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,66 @@
+import clsx from 'clsx';
+import type { ReactNode } from 'react';
+import React from 'react';
+
+type Props = {
+  variant?: 'fillPrimary' | 'fillNeutral' | 'outlinePrimary' | 'outlineNeutral';
+  size?: 'large' | 'medium' | 'small';
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({
+  variant = 'fillPrimary',
+  size = 'large',
+  children,
+  leftIcon,
+  rightIcon,
+  ...restProps
+}: Props) => {
+  const base = 'flex items-center gap-1 typo-body1-medium disabled:cursor-not-allowed';
+
+  const variantClass = {
+    fillPrimary: 'bg-brand-700 text-white disabled:bg-brand-400',
+    fillNeutral: 'bg-neutral-200 text-neutral-600',
+    outlinePrimary:
+      'bg-white border border-brand-700 text-brand-700 disabled:border-brand-400 disabled:text-brand-400',
+    outlineNeutral: 'bg-white border border-neutral-200 text-neutral-600',
+  }[variant];
+
+  const sizeClass = {
+    large: 'h-12 px-4 rounded-xl',
+    medium: 'h-11 px-4 rounded-xl',
+    small: 'h-9 px-3 rounded-lg',
+  }[size];
+
+  const leftIconClass = {
+    large: 'pl-3',
+    medium: 'pl-3',
+    small: 'pl-2',
+  }[size];
+
+  const rightIconClass = {
+    large: 'pr-3',
+    medium: 'pr-3',
+    small: 'pr-2',
+  }[size];
+
+  return (
+    <button
+      className={clsx(
+        base,
+        variantClass,
+        sizeClass,
+        !!leftIcon && leftIconClass,
+        !!rightIcon && rightIconClass,
+      )}
+      {...restProps}
+    >
+      {leftIcon}
+      {children}
+      {rightIcon}
+    </button>
+  );
+};
+
+export default Button;
