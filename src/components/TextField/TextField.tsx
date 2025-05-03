6423d987c2ec146e0759b3c4d85f25830f545565
HyeBinYang <skdisk7368@gmail.com>
2025-05-03 19:10:30 +0900

    fix: TextField 컴포넌트 padding 설정 수정

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -3,6 +3,7 @@
 import clsx from 'clsx';
 import type { ChangeEventHandler, ComponentPropsWithoutRef, ReactNode, Ref } from 'react';
 import React, { forwardRef, useId, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 import type { Color } from '@/types/common';
 
@@ -55,19 +56,21 @@ const TextField = forwardRef<HTMLInputElement | HTMLTextAreaElement, Props>(
         )}
         <div
           className={clsx(
-            'typo-body1-regular flex items-center gap-2 rounded border bg-white py-3 pl-3 text-neutral-800',
-            multiLine ? 'h-[140px]' : 'h-14 pr-3',
-            error ? 'border-error-600' : 'border-neutral-300',
-            inputProps.disabled && 'cursor-not-allowed border-neutral-300 bg-neutral-100',
-            'focus-within:border-brand-700',
-            inputProps.className,
+            'typo-body1-regular flex items-center gap-2 bg-white text-neutral-800',
+            multiLine ? 'h-[140px]' : 'h-14',
           )}
         >
           {multiLine ? (
             <textarea
               ref={ref as Ref<HTMLTextAreaElement>}
-              className={clsx(
-                'h-full w-full resize-none pr-3 outline-none scrollbar scrollbar-thin scrollbar-track-transparent scrollbar-thumb-neutral-300',
+              className={twMerge(
+                clsx(
+                  'h-full w-full resize-none rounded border px-3 py-3 placeholder-neutral-800 outline-none scrollbar scrollbar-thin scrollbar-track-transparent scrollbar-thumb-neutral-300',
+                  'disabled:cursor-not-allowed disabled:bg-neutral-100 disabled:placeholder-neutral-500',
+                  'focus:border-brand-700',
+                  error ? 'border-error-600' : 'border-neutral-300',
+                  inputProps.className,
+                ),
               )}
               {...(inputProps as ComponentPropsWithoutRef<'textarea'>)}
               onChange={handleChange}
@@ -79,9 +82,14 @@ const TextField = forwardRef<HTMLInputElement | HTMLTextAreaElement, Props>(
                 type="text"
                 {...(inputProps as ComponentPropsWithoutRef<'input'>)}
                 id={inputProps?.id ?? defaultId}
-                className={clsx(
-                  'w-full flex-1 appearance-none placeholder-neutral-800 outline-none',
-                  'disabled:cursor-not-allowed disabled:placeholder-neutral-500',
+                className={twMerge(
+                  clsx(
+                    'h-full w-full flex-1 appearance-none rounded border px-3 py-3 placeholder-neutral-800 outline-none',
+                    'disabled:cursor-not-allowed disabled:bg-neutral-100 disabled:placeholder-neutral-500',
+                    'focus:border-brand-700',
+                    error ? 'border-error-600' : 'border-neutral-300',
+                    inputProps.className,
+                  ),
                 )}
                 onChange={handleChange}
               />
@@ -90,13 +98,13 @@ const TextField = forwardRef<HTMLInputElement | HTMLTextAreaElement, Props>(
           )}
         </div>
         {inputProps.maxLength && (
-          <p className={`text-${error ? 'error-600' : 'neutral-800'} typo-element3 self-end`}>
+          <p className={`${error ? 'text-error-600' : 'text-neutral-800'} typo-element3 self-end`}>
             {textLength}/{inputProps.maxLength}
           </p>
         )}
         {helperText?.value && (
           <p
-            className={`text-${error ? 'error-600' : (helperText?.color ?? 'neutral-800')} typo-element3`}
+            className={`${error ? 'text-error-600' : (helperText?.color ?? 'text-neutral-800')} typo-element3`}
           >
             {helperText.value}
           </p>
