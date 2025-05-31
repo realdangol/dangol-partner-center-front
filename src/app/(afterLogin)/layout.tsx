2c13494d0fe557f4c735e69c548be7111247c10f
HyeBinYang <skdisk7368@gmail.com>
2025-05-31 20:11:58 +0900

    feat: 인증 처리 로직 제거 (테스트 용)

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -2,7 +2,6 @@ import type { PropsWithChildren } from 'react';
 import React from 'react';
 
 import { AuthProvider } from '@/components';
-import withAuth from '@/hoc/withAuth';
 
 import { Header, Sidebar } from './_components';
 
@@ -24,4 +23,4 @@ const AfterLoginLayout = ({ children, accessToken }: PropsWithChildren<Props>) =
   );
 };
 
-export default withAuth(AfterLoginLayout);
+export default AfterLoginLayout;
